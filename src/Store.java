7b11ad62f272b8ba47ab4dcb86de6252674a14a0
Joan_Monroy <jdmonroyq@ufpso.edu.co>
2024-09-14 16:11:06 -0500

    Refactor complete for quiz

--- src/Store.java
+++ src/Store.java
@@ -6,21 +6,22 @@ public class Store {
         final int PRODUCT_PRECE_2 = 10;
         final int PRODUCT_PRECE_3 = 5;
 
-        final int QUANTITIES_PRODUCT_1 = 2;
-        final int QUANTITIES_PRODUCT_2 = 3;
-        final int QUANTITIES_PRODUCT_3 = 4;
+        final int PROMEDIO_VENTAS = 50;
+
+        final int QUANTITIES_PRODUCT_1 = 1;
+        final int QUANTITIES_PRODUCT_2 = 1;
+        final int QUANTITIES_PRODUCT_3 = 1;
 
         int[] productsPrices = {PRODUCT_PRECE_1,PRODUCT_PRECE_2,PRODUCT_PRECE_3};
         int[] productsQuantities = {QUANTITIES_PRODUCT_1,QUANTITIES_PRODUCT_2,QUANTITIES_PRODUCT_3}; 
 
-        for ()
-        int total1 = p1 * 2;
-        int total2 = p2 * 3;
-        int total3 = p3 * 4;
+        int totalSales = 0;
 
-        int totalSales = total1 + total2 + total3;
+        for (int i = 0; i<productsPrices.length; i++){
+            totalSales += productsPrices[i] * productsQuantities[i];
+        }
 
-        if (totalSales > 50) {
+        if (totalSales > PROMEDIO_VENTAS) {
             System.out.println("Good sales performance");
         } else {
             System.out.println("Low sales performance");
